281d013fe8972f36dbee56b09d5819f11124e1e2
Romuald Brillout <github.com@brillout.com>
2020-03-19 23:49:43 +0100

    improve pretty_scroll_area - backwards incompatible

--- pretty_scroll_area.js
+++ pretty_scroll_area.js
@@ -3,37 +3,52 @@ import assert from '@brillout/assert';
 
 export default pretty_scroll_area;
 
-export {getScroll, setScroll, scrollToElement};
+export {getScroll, setScroll, scrollToElement, addScrollListener, removeScrollListener};
 
+/*
+const DISABLE = true;
+/*/
+const DISABLE = false;
+//*/
 
 let scroll_el;
 
 const hide_scroll_state = {
-  is_on_top: null,
+  is_on_hide_scroll_element: null,
   enable_scroll_auto_hide: null,
   scrollbar_width_computed: null,
 };
 function onStateChange() {
   const hide_scroll = (
-    hide_scroll_state.is_on_top &&
+    hide_scroll_state.is_on_hide_scroll_element &&
     hide_scroll_state.enable_scroll_auto_hide &&
     hide_scroll_state.scrollbar_width_computed
   );
+  if( DISABLE ){
+    return;
+  }
   scroll_el.classList[hide_scroll?'add':'remove']('hide_scroll');
 }
 
+let initialized = false;
 function pretty_scroll_area() {
+  if( initialized ) return;
+  initialized = true;
+
   const s1 = document.querySelectorAll('.pretty_scroll_area');
   const s2 = document.querySelectorAll('.pretty_scroll_area__parent');
-  assert.usage(s1.length===1 && s2.length===1);
+  const s3 = document.querySelectorAll('.pretty_scroll_area__hide_scroll_element');
+  assert.usage(s1.length===1 && s2.length===1 && s3.length===1);
   scroll_el = s1[0];
+  const hide_scroll_element = s3[0];
 
   compute_scrollbar_width();
   hide_scroll_state.scrollbar_width_computed = true;
   onStateChange();
 
-  add_on_scroll_listener(scroll_pos => {
-    hide_scroll_state.is_on_top = scroll_pos === 0;
+  addScrollListener(scroll_pos => {
+    const hide_scroll_pos = scroll_pos === getElementScroll(hide_scroll_element);
+    hide_scroll_state.is_on_hide_scroll_element = hide_scroll_pos;
     onStateChange();
   });
 
@@ -43,27 +58,48 @@ function pretty_scroll_area() {
   }, 2000);
 }
 
-function add_on_scroll_listener(on_scroll) {
-  call();
-
+const scrollListeners = [];
+var isAutoScrolling;
+function addScrollListener(scrollListener, {onlyUserScroll}={}) {
+  scrollListener.onlyUserScroll = onlyUserScroll;
+  scrollListeners.push(scrollListener);
+  add_global_scroll_listener();
+}
+function removeScrollListener(scrollListener) {
+  const idx = scrollListeners.indexOf(scrollListener);
+  if( idx !== -1 ){
+    scrollListeners.splice(idx, 1);
+  }
+}
+let already_listening = false;
+function add_global_scroll_listener() {
+  if( already_listening ) return;
+  pretty_scroll_area();
+  already_listening = true;
   const scroll_event_el = (
     scroll_el === document.documentElement ?
       window :
       scroll_el
   );
-
-  scroll_event_el.addEventListener('scroll', call, {passive: true});
-
-  function call() {
-    const scroll_pos = getScroll();
-    on_scroll(scroll_pos);
-  }
+  scroll_event_el.addEventListener(
+    'scroll',
+    () => {
+      const scroll_pos = getScroll();
+      scrollListeners.forEach(scrollListener => {
+        if( scrollListener.onlyUserScroll && isAutoScrolling ){
+          return;
+        }
+        scrollListener(scroll_pos);
+      });
+    },
+    {passive: true},
+  );
 }
 
 function getScroll() {
   return scroll_el.scrollTop;
 }
-function setScroll(newTop, {smooth=false}={}) {
+async function setScroll(newTop, {smooth=false}={}) {
   if( !smooth ){
     return jumpTo(newTop);
   } else {
@@ -90,12 +126,17 @@ async function slideTo(top) {
   const currentTop = getScroll();
   const distance = top - currentTop;
 
+  isAutoScrolling = true;
   let counter = 0;
   var timer = setInterval(function () {
     jumpTo(currentTop + distance * smoothStep(counter++ / STEPS));
-    if (counter > STEPS)
+    if (counter > STEPS) {
       clearInterval(timer);
+      window.requestAnimationFrame(() => {
+        isAutoScrolling = false;
+      });
       resolvePromise();
+    }
   }, INTERVAL);
 
   return promise;
@@ -105,14 +146,19 @@ async function slideTo(top) {
   }
 }
 
-function scrollToElement(selector) {
-  const el = document.querySelector(selector);
-//const top = el.getBoundingClientRect().top;
-  const top = el.getBoundingClientRect().top + getScroll();
-  setScroll(top, {smooth: true});
-//scroll_el.scrollBy({top, behavior: 'smooth'});
-//scroll_el.scrollTo({top, behavior: 'smooth'});
-//setScroll(top);
+function scrollToElement(selector_or_element) {
+  const top = getElementScroll(selector_or_element);
+  return setScroll(top, {smooth: true});
+}
+
+function getElementScroll(selector_or_element) {
+  const element = (
+    selector_or_element.constructor === String ?
+      document.querySelector(selector_or_element) :
+      selector_or_element
+  );
+  const top = element.getBoundingClientRect().top + getScroll();
+  return top;
 }
 
 
