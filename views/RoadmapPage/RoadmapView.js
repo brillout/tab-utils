83e9cdefbfdc1de53a45005cc3f4a866f9cc5886
Romuald Brillout <github.com@brillout.com>
2020-04-08 16:11:41 +0200

    make common code generic

--- views/RoadmapPage/RoadmapView.js
+++ views/RoadmapPage/RoadmapView.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {tab_app_roadmap} from '../../../tab_app_info';
+import {TabAppRoadmap} from '../../../tab_app_info';
 
 export default RoadmapView;
 
 function RoadmapView() {
   return <>
-    <ul>{tab_app_roadmap}</ul>
+    <ul>
+      <TabAppRoadmap />
+    </ul>
     <p>
-    You have wish? <a href="/feature-suggestion">Suggest a feature</a>!
+      You have wish? <a href="/feature-suggestion">Suggest a feature</a>!
     </p>
   </>;
 }
