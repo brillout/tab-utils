44cf42507e7db779bb273d470a34d2182f335497
Romuald Brillout <github.com@brillout.com>
2020-04-28 11:17:00 +0200

    [code-removal] implement new ad code

--- load_ad.js
+++ load_ad.js
@@ -1,3 +1,4 @@
+import "./ad_layout.css";
 import React from "react";
 import assert from "@brillout/assert";
 import load_script from "./private/load_script";
@@ -10,141 +11,58 @@ export { Ad_ATF, Ad_BTF };
 
 export { ads_are_removed };
 
+function Ad_ATF({ ad_slots }) {
+  return <AdView ad_slots={ad_slots} slot_name={"ATF"} />;
+}
+function Ad_BTF({ ad_slots }) {
+  return <AdView ad_slots={ad_slots} slot_name={"BTF"} />;
+}
+
+function AdView({ id, ad_slots, slot_name }) {
+  assert(["ATF", "BTF"].includes(slot_name));
+  const slots = ad_slots.filter((slot) => slot.slot_name === slot_name);
+  assert(ad_slots.length === 2);
+  assert(slots.length === 1);
+  const { slot_id } = slots[0];
+  assert(slot_id);
+  return (
+    <div className="ad-view">
+      <ins
+        className="adsbygoogle"
+        data-ad-client="ca-pub-6953474219479917"
+        data-full-width-responsive="true"
+        data-ad-slot={slot_id}
+      ></ins>
+      <a className="ad_remover" href="donate" target="_blank">
+        Remove ad
+      </a>
+    </div>
+  );
+}
+
 function load_ad(AD_SLOTS) {
-  return;
   if (ads_are_removed()) {
     return;
   }
 
-  load_header_bidding();
-
-  loadAdsByGoogle();
-
-  loadGoogleTag(AD_SLOTS);
-
-  loadApsTag();
-
-  fetch_bids(AD_SLOTS, { is_first_time: true });
-
-  AD_SLOTS.forEach(({ slotID }) => {
-    googletag.cmd.push(function () {
-      googletag.display(slotID);
-    });
-  });
-
-  setInterval(function () {
-    fetch_bids(AD_SLOTS);
-  }, 90000);
+  loadAdsByGoogle(AD_SLOTS);
 }
 
-function load_header_bidding() {
-  load_script("https://go.automatad.com/geo/5fkVze/afihbs.js");
+/*
+declare global {
+  interface Window {
+    adsbygoogle: any;
+  }
 }
-
-function loadAdsByGoogle() {
+*/
+function loadAdsByGoogle(AD_SLOTS) {
   const scriptEl = load_script(
     "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
   );
-  scriptEl.setAttribute("data-ad-client", "ca-pub-3994464140431603");
-}
-
-function loadGoogleTag(AD_SLOTS) {
-  load_script("https://securepubads.g.doubleclick.net/tag/js/gpt.js");
-
-  window.googletag = window.googletag || { cmd: [] };
-  googletag.cmd.push(function () {
-    const size_map = googletag
-      .sizeMapping()
-      .addSize([728, 0], [728, 90]) //desktop
-      .addSize([0, 0], [320, 50]) //other
-      .build();
-
-    AD_SLOTS.forEach(({ slotID, slotName, slotSizes }) => {
-      googletag
-        .defineSlot(slotName, slotSizes, slotID)
-        .defineSizeMapping(size_map)
-        .addService(googletag.pubads());
-
-      console.log(
-        "[AD] Defined ad with id " +
-          slotID +
-          " name " +
-          slotName +
-          " and sizes " +
-          slotSizes
-      );
-    });
-
-    googletag.pubads().disableInitialLoad();
-    // googletag.pubads().enableSingleRequest();
-    googletag.enableServices();
+  window.adsbygoogle = window.adsbygoogle || [];
+  AD_SLOTS.forEach(() => {
+    window.adsbygoogle.push({});
   });
-}
-
-function fetch_bids(AD_SLOTS, { is_first_time = false } = {}) {
-  const slots = AD_SLOTS.map(({ slotID, slotName, slotSizes }) => {
-    return {
-      slotID,
-      slotName,
-      sizes: slotSizes,
-    };
-  });
-  const bid_args = { slots };
-  if (!is_first_time) {
-    bid_args.timeout = 2e3;
-  }
-  apstag.fetchBids(bid_args, function () {
-    googletag.cmd.push(function () {
-      apstag.setDisplayBids();
-      if (!is_first_time) {
-        googletag.pubads().refresh();
-      }
-      console.log("[AD] bid " + (is_first_time ? "init" : "refresh"));
-    });
-  });
-}
-
-function loadApsTag() {
-  !(function (a9, a, p, s, t, A, g) {
-    if (a[a9]) return;
-    function q(c, r) {
-      a[a9]._Q.push([c, r]);
-    }
-    a[a9] = {
-      init: function () {
-        q("i", arguments);
-      },
-      fetchBids: function () {
-        q("f", arguments);
-      },
-      setDisplayBids: function () {},
-      targetingKeys: function () {
-        return [];
-      },
-      _Q: [],
-    };
-    A = p.createElement(s);
-    A.async = !0;
-    A.src = t;
-    g = p.getElementsByTagName(s)[0];
-    g.parentNode.insertBefore(A, g);
-  })(
-    "apstag",
-    window,
-    document,
-    "script",
-    "//c.amazon-adsystem.com/aax2/apstag.js"
-  );
-
-  const pubID = "f6d6bc2e-7f12-42a3-83a2-6035f3b14586";
-
-  apstag.init({
-    pubID,
-    adServer: "googletag",
-    bidTimeout: 2e3,
-  });
-
-  console.log("[AD] ApsTag initialized with pubID " + pubID);
 }
 
 // Since Clock/Timer Tab's source code is open anyone can read this and bypass doing a donation to remove ads.
@@ -155,49 +73,18 @@ function ads_are_removed() {
   const code_in_storage = store.has_val(AD_REMOVAL_KEY);
 
   if (window.location.hash === "#thanks-for-your-donation") {
-    if( !code_in_storage  ){
+    if (!code_in_storage) {
       store.set_val(AD_REMOVAL_KEY, true);
     }
     remove_hash();
     return true;
   }
 
-  if( code_in_storage ){
+  if (code_in_storage) {
     return true;
   }
 
   document.documentElement.classList.add("show_ad");
 
   return false;
-}
-
-function Ad_ATF({ ad_slots }) {
-  const slots = ad_slots.filter((slot) => slot.slotName.includes("ATF"));
-  assert(ad_slots.length === 2);
-  assert(slots.length === 1);
-  return <AdView id="primary-ad" slot={slots[0]} />;
-}
-function Ad_BTF({ ad_slots }) {
-  const slots = ad_slots.filter((slot) => slot.slotName.includes("BTF"));
-  assert(ad_slots.length === 2);
-  assert(slots.length === 1);
-  return <AdView id="secondary-ad" slot={slots[0]} />;
-}
-
-function AdView({ id, slot: { slotName, slotID } }) {
-  assert(slotName && slotID);
-  return (
-    <div id={id}>
-      <div className="ad-content-wrapper">
-        {
-          <div id={slotName}>
-            <div id={slotID} />
-          </div>
-        }
-      </div>
-      <a className="ad_remover" href="donate" target="_blank">
-        Remove ad
-      </a>
-    </div>
-  );
 }
