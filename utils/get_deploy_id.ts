47c6d9ad805389dd39128aa7e681183a1c0e13c9
Romuald Brillout <github.com@brillout.com>
2020-11-05 09:59:12 +0100

    make deploy ID accessible over window obj

--- utils/get_deploy_id.ts
+++ utils/get_deploy_id.ts
@@ -4,6 +4,16 @@ const DEPLOY_ID = "__DEPLOY";
 
 export { get_deploy_id };
 
+declare global {
+  interface Window {
+    deployId: string;
+    DEPLOY_ID: string;
+  }
+}
+if (typeof window !== "undefined") {
+  window.deployId = window.DEPLOY_ID = DEPLOY_ID;
+}
+
 function get_deploy_id() {
   // In order to ensure source mapping, we ensure that the placeholder
   // string got replaced with a same-sized ID string.
